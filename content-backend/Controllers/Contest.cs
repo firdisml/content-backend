1e96e7ce3b987f487b1ffa9357df2bdcacb398e8
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-02-27 17:44:26 +0800

    Add project files.

---
+++ content-backend/Controllers/Contest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace content_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Contest : ControllerBase
+    {
+    }
+}
